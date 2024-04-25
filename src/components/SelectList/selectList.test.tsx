840a9187467f20476f055c1a899ad6fd238d61b3
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-04-25 20:55:35 +0100

    tests

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+import { SelectList } from "@components/SelectList";
+
+describe("Component: SelectList", () => {
+  it("should be return city details selected", () => {
+    const data = [
+      { id: "1", name: "Campinas", latitude: 123, longitude: 456 },
+      { id: "2", name: "Campo Grande", latitude: 789, longitude: 987 },
+    ];
+    const onPress = jest.fn();
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />);
+
+    const selectedCity = screen.getByText(/campo/i);
+    fireEvent.press(selectedCity);
+    // how many times the event was clicked
+    //expect(onPress).toHaveBeenCalledTimes(1)
+    expect(onPress).toBeCalledWith(data[1]);
+  });
+});
